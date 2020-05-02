1ad5f32647160da533f9c2b33841ba61eaf3b658
raudelf <rawdell@outlook.com>
2020-05-01 20:42:08 -0500

    finished the register endpoint and tested

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('./auth-model.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(creds => {
+      res.status(201).json({creds});
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'There was a problem with the db', err});
+    });
 });
 
 router.post('/login', (req, res) => {
