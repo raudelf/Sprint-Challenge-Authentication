08ad5fd6b7e5849a5a3d2382fb61eef27e892d45
raudelf <rawdell@outlook.com>
2020-05-01 21:01:51 -0500

    created the authentication middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,23 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  try {
+    const token = req.headers.authorization;
+
+    if (token) {
+      jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        if (err) {
+          throw new Error(err)
+        } else {
+          req.decodedToken = decodedToken;
+          next();
+        };
+      })
+    } else {
+      throw new Error('Bad Auth');
+    };
+  } catch (err) {
+    res.status(401).json(err.message);
+  };
 };
